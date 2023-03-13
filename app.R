4ee50f1139c8a2bac92c0d7801feff76e5aed322
tapstheta <127714129+tapstheta@users.noreply.github.com>
2023-03-13 01:28:57 -0400

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,29 @@ library(shiny)
 library(ggplot2)
 library(plotly)
 
+#dataset
 babies = read.csv("https://uwmadison.box.com/shared/static/7os6119jv2z2auql9tilkp4xv8vff2u1.csv")
+
+# Define the dotplot function
+dotplot <- function(df, y_var, smoke) {
+  if (smoke) {
+    # Plot smoking data
+    df_sub <- subset(df, smoke == 1)
+    p <- ggplot(df_sub, aes(x = age, y = !!sym(y_var), color = parity)) +
+      geom_point() +
+      labs(x = "Age", y = y_var, color = "Parity") +
+      theme_bw()
+  } else {
+    # Plot non-smoking data
+    df_sub <- subset(df, smoke == 0)
+    p <- ggplot(df_sub, aes(x = age, y = !!sym(y_var), color = parity)) +
+      geom_point() +
+      labs(x = "Age", y = y_var, color = "Parity") +
+      theme_bw()
+  }
+  ggplotly(p, tooltip = "text") %>%
+    style(hoveron = "fill")
+}
 
 #output text for ui
 maintext = p("This dataset is from between 1960 and 1967 among women in the San Francisco East Bay area. It encompasses informationb about their pregnancy as well as stats about babies immediatelty after birth. Specifically, age represent the age of the mother at the time of birth. bwt is the weight of the baby at birth. Parity is whether or not the baby is the first child. Smoke is whether the mother is a smoker. gestation represent the gestation period for a specific baby.")
@@ -16,7 +38,7 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("y_var", "Y-axis variable:",
                   choices = c("bwt", "gestation")),
-      checkboxInput("smoke", "Eliminate non-smoking mothers")
+      checkboxInput("smoke", "Select for data on mothers who smoke")
     ),
     # Output plot
     mainPanel(
@@ -28,27 +50,6 @@ ui <- fluidPage(
 
 # Define the server
 server <- function(input, output) {
-  
-  # Define the dotplot function
-  dotplot <- function(df, y_var, smoke) {
-    if (smoke) {
-      # Plot smoking data
-      df_sub <- subset(df, smoke == 1)
-      p <- ggplot(df_sub, aes(x = age, y = !!sym(y_var), color = parity)) +
-        geom_point() +
-        labs(x = "Age", y = y_var, color = "Parity") +
-        theme_bw()
-    } else {
-      # Plot non-smoking data
-      df_sub <- subset(df, smoke == 0)
-      p <- ggplot(df_sub, aes(x = age, y = !!sym(y_var), color = parity)) +
-        geom_point() +
-        labs(x = "Age", y = y_var, color = "Parity") +
-        theme_bw()
-    }
-    ggplotly(p, tooltip = "text") %>%
-      style(hoveron = "fill")
-  }
   
   
   # Render the dotplot output
@@ -63,4 +64,5 @@ server <- function(input, output) {
 
 # Run the app
 shinyApp(ui = ui, server = server)
+
 
